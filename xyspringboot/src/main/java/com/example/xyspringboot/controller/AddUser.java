a7aae47336057424e3bb5647909e43665291e8e0
lixiangyun <575559167@qq.com>
2023-03-12 09:49:27 +0800

    AddUser提交

---
+++ xyspringboot/src/main/java/com/example/xyspringboot/controller/AddUser.java
@@ -0,0 +1,11 @@
+package com.example.xyspringboot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/AddUser")
+public class AddUser {
+
+}
